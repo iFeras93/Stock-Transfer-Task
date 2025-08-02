ed2140338ff0bd9d7b7435b76c10081b3469a1c1
Feras Mohamed <ferasmo7mad@gmail.com>
2025-08-02 20:16:10 +0300

    wip: update api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,29 @@
 <?php
 
+use App\Http\Controllers\API\V1\AuthController;
+use App\Http\Controllers\API\V1\StockTransferController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['prefix' => 'v1'], function () {
+    // Authentication routes
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+
+    Route::middleware(['auth:sanctum'])->group(function () {
+        // Auth user routes
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('user', [AuthController::class, 'user']);
+
+        Route::group(['prefix' => 'stock_transfers'], function () {
+            Route::get('index', [StockTransferController::class, 'index']);
+            Route::get('statusFilter', [StockTransferController::class, 'statusFilter']);
+            Route::post('store', [StockTransferController::class, 'store']);
+            Route::post('{stockTransfer}/change_status', [StockTransferController::class, 'changeStatus']);
+            Route::get('{stockTransfer}/info_details', [StockTransferController::class, 'infoDetails']);
+            Route::post('{stockTransfer}/cancel_or_return', [StockTransferController::class, 'cancelOrReturn']);
+        });
+    });
+
+});
